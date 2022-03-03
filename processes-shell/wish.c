7307eb6b068f54900f72b18418a03a72bf93f389
Abdallah Ahmed <abdallahar1974@gmail.com>
2022-03-04 01:18:00 +0200

    feat: split the input command into tokens

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -4,9 +4,37 @@
 #include <string.h>
 #include "vector.h"
 
-void execute_command (char* line) {
-    if (strcmp(line, "exit") == 0) exit(0);
-    printf("%s\n", line);
+/* Given a line, return a vector of space-separated tokens */
+Vector getTokens (char* l) {
+    char* line = (char*) malloc( (strlen(l) + 1) * sizeof(char));
+    strcpy(line, l);
+    Vector ans = create_vector();
+    char* token = NULL;
+    while (line != NULL) {
+        while(*line == ' ') line++;
+        token = strsep(&line, " ");
+        push_back(&ans, token);
+    }
+    return ans;
+}
+
+/* Given a string, removes leading and trailing white spaces */
+char* trim (char* line) {
+    int l = 0, r = strlen(line) - 1;
+    while (line[l] == ' ') l++;
+    while (line[r] == ' ') r--;
+    int length = r - l + 1;
+    char* ans = (char*) malloc((length + 1) * sizeof(char));
+    strncpy(ans, line+l, length);
+    ans[length] = '\0';
+    return ans;
+}
+
+
+void execute_command (Vector line) {
+    if (line.size == 1 && strcmp(get(&line, 0), "exit") == 0) exit(0);
+    for (int i = 0; i < line.size; i++) printf("%s ", get(&line, i));
+    printf("\n");
 }
 
 int main (int argc, char* argv[])
@@ -19,7 +47,7 @@ int main (int argc, char* argv[])
         size_t n = 0;
         int len = getline(&line, &n, stdin);
         line[len-1] = '\0';
-        execute_command(line);
+        execute_command(getTokens(trim(line)));
     }
 
 return 0;

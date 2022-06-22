bf353af6a95647306dc5031f1a3eef8e39c195f1
Abdallah Ahmed <abdallahar1974@gmail.com>
2022-06-22 04:17:33 +0200

    Make vector_t thread-safe by adding a lock

--- concurrency-mapreduce/vector.c
+++ concurrency-mapreduce/vector.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 #include "vector.h"
 
@@ -12,6 +13,7 @@ vector_t* make_vector () {
     }
     v->size = 0;
     v->capacity = INITIAL_CAPACITY;
+    pthread_mutex_init(&v->lock, NULL);
     v->data = malloc(sizeof(void*) * INITIAL_CAPACITY);
     if (v->data == NULL) {
         printf("Error during vector creation\n");
@@ -20,7 +22,7 @@ vector_t* make_vector () {
     return v;
 }
 
-void grow (vector_t *v) {
+static void grow (vector_t *v) {
     void** new_arr = (void**) malloc(sizeof(void*) * v->capacity * 2);
     for (int i = 0; i < v->size; i++)
         new_arr[i] = v->data[i];
@@ -30,20 +32,25 @@ void grow (vector_t *v) {
 }
 
 void vector_push (vector_t* v, void* element) {
+    pthread_mutex_lock(&v->lock);
     if (v->capacity == v->size) grow(v);
     v->data[v->size] = element;
     v->size++;
+    pthread_mutex_unlock(&v->lock);
 }
 
 void vector_pop (vector_t* v) {
+    pthread_mutex_lock(&v->lock);
     if (v->size == 0) {
         printf("Error: Vector is empty\n");
         exit(1);
     }
     v->size--;
+    pthread_mutex_unlock(&v->lock);
 }
 
 void vector_sort (vector_t* v, int (*compare)(const void*, const void*)) {
+    pthread_mutex_lock(&v->lock);
     for (int i = 0; i < v->size; i++) {
         for (int j = i+1; j < v->size; j++) {
             if (compare(v->data[i], v->data[j]) == 0) {
@@ -53,15 +60,18 @@ void vector_sort (vector_t* v, int (*compare)(const void*, const void*)) {
             }
         }
     }
+    pthread_mutex_unlock(&v->lock);
 }
 
 void vector_reverse (vector_t* v) {
+    pthread_mutex_lock(&v->lock);
     for (int i = 0; i < v->size / 2; i++) {
         int j = v->size - 1 - i;
         void* tmp = v->data[i];
         v->data[i] = v->data[j];
         v->data[j] = tmp;
     }
+    pthread_mutex_unlock(&v->lock);
 }
 
 static int compare_int (const void* a, const void* b) {
